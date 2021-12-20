e81ccc08b92de49160474da830a334f2ee8f07ac
markoter <61563247+markoter@users.noreply.github.com>
2021-12-20 17:27:45 +0100

    add jokes to the navbar

--- Data/Migrations/20211220160006_initialsetup.Designer.cs
+++ Data/Migrations/20211220160006_initialsetup.Designer.cs
@@ -4,14 +4,16 @@ using JokesWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JokesWeb.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211220160006_initialsetup")]
+    partial class initialsetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
